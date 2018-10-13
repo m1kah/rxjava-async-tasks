af3a1ca662974cf6e9b47682500ef9600dcafb55
Mika Hamalainen <mika.hamalainen@gmail.com>
2018-10-13 12:37:57 +0300

    Do revenue calculation in main thread

--- src/main/java/com/m1kah/rx/TicketService.java
+++ src/main/java/com/m1kah/rx/TicketService.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import rx.Observable;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 public class TicketService {
     private static final Logger log = LoggerFactory.getLogger(TicketService.class);
@@ -16,12 +17,18 @@ public class TicketService {
     }
 
     Observable<BigDecimal> revenueForFilm(String film) {
-        return ticketRepository.find(film)
-                .reduce(
-                        new BigDecimal("0.00"),
-                        (revenue, ticket) -> {
-                            log.info("revenue: {}, price: {}", revenue, ticket.getPrice());
-                            return revenue.add(ticket.getPrice());
-                        });
+        // Call repository. We know that it will execute query in io scheduler
+        // and result will be passed back in computation scheduler. Wait
+        // for result in calling thread.
+        List<Ticket> tickets = ticketRepository.find(film).toList().toBlocking().first();
+        // Return calculation observable. Calculation will be completed
+        // in calling thread.
+        return Observable.from(tickets)
+                    .reduce(
+                    new BigDecimal("0.00"),
+                    (revenue, ticket) -> {
+                        log.info("revenue: {}, price: {}", revenue, ticket.getPrice());
+                        return revenue.add(ticket.getPrice());
+                    });
     }
 }
